1d10768033d651cbc1cadd1d0419bd1672a6fa62
swleeee <42002888+swleeee@users.noreply.github.com>
2022-02-19 11:20:34 +0900

    기능 추가 : nav 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import styled from 'styled-components';
+import app from './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Nav />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  // background-color: green;
+
+  width: 100%;
+`;
---
+++ src/components/Nav.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+// class Nav extends Component {
+//   render() {
+//     return <div>ddd</div>;
+//   }
+// }
+
+function Nav() {
+  return (
+    <Container>
+      <Home>
+        <div>SWLEE</div>
+      </Home>
+      <MenuBox>
+        <div>SKILL</div>
+        <div>PROJECT</div>
+        <div>CONTACT</div>
+      </MenuBox>
+    </Container>
+  );
+}
+
+export default Nav;
+
+const Container = styled.div`
+  //   background-color: #733c78;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 3px solid black;
+
+  @media (min-width: 992px) and (max-width: 1299.98px) {
+  }
+
+  @media (min-width: 1300px) {
+    width: 1300px;
+  }
+`;
+
+const Home = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  //   background-color: skyblue;
+  //   font-size: 30px;
+
+  div {
+    font-family: Hyemin_Bold;
+    font-size: 30px;
+  }
+`;
+const MenuBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div {
+    margin: 0 10px;
+    font-family: Hyemin_Bold;
+    font-size: 24px;
+  }
+`;
